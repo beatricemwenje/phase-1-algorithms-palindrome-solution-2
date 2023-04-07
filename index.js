33c01f1c050efdca7dcc09fd3e07f2a97200d842
beatrice <beatrice.mwenje@student.moringaschool>
2023-04-07 22:58:53 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  function isPalindrome(word) {
+    for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+      const endIndex = word.length - 1 - startIndex;
+      if (word[startIndex] !== word[endIndex]) {
+        return false;
+      }
+    }
 
+    return true;
+  }
+}
 /* 
   Add your pseudocode here
+  lets iterate from the beginning of the string to the middle of the string.
+  Then compare the letter we're iterating over to the corresponding letter at the end of the string.
+  If the letters don't match, return false and if we reach the middle, and all the letters match, return true
 */
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
